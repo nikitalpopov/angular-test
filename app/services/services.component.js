e4461f130798cdeb6f8ee2ad8254de25f8b07842
nikitalpopov <tapochek97@ya.ru>
2017-10-05 19:18:05 +0300

    second commit

--- app/services/services.component.js
+++ app/services/services.component.js
@@ -7,23 +7,40 @@ component('services', {
             {
                 name: 'Web Development',
                 price: 300,
-                currency: '$'
+                currency: '$',
+                isChosen: false
             },
             {
                 name: 'Design',
                 price: 400,
-                currency: '$'
+                currency: '$',
+                isChosen: false
             },
             {
                 name: 'Integration',
                 price: 250,
-                currency: '$'
+                currency: '$',
+                isChosen: false
             },
             {
                 name: 'Training',
                 price: 220,
-                currency: '$'
+                currency: '$',
+                isChosen: false
             }
         ];
+
+        this.total = 0;
+
+        var self = this;
+
+        self.updateTotal = function setTotal(service) {
+            service.isChosen = !service.isChosen;
+            if (service.isChosen)
+                self.total += service.price;
+            else
+                self.total -= service.price;
+        };
+
     }
 });
